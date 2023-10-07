f8a5c9fc80a0033c1346ec88ae7670b738774880
Serhii Stanislav <serhiistanislav@gmail.com>
2023-10-07 18:46:27 +0300

    ADD | favicon

--- lib/serverClient.ts
+++ lib/serverClient.ts
@@ -5,26 +5,16 @@ import {
 } from "@apollo/experimental-nextjs-app-support/ssr";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
-const { getClient: getServerClient } = registerApolloClient(() => {
-  const apolloClientUrl = process.env.APOLLO_CLIENT_URL;
+const apolloClientUrl = process.env.APOLLO_CLIENT_URL;
 
-  return new NextSSRApolloClient({
-    cache: new NextSSRInMemoryCache(),
-    link: new HttpLink({
-      uri: apolloClientUrl,
-    }),
-  });
-});
+const { getClient: getServerClient } = registerApolloClient(
+  () =>
+    new NextSSRApolloClient({
+      cache: new NextSSRInMemoryCache(),
+      link: new HttpLink({
+        uri: apolloClientUrl,
+      }),
+    })
+);
 
 export default getServerClient;
-
-// export const { getClient } = registerApolloClient(() => {
-//   const apolloClientUrl = process.env.APOLLO_CLIENT_URL;
-
-//   return new NextSSRApolloClient({
-//     cache: new NextSSRInMemoryCache(),
-//     link: new HttpLink({
-//       uri: apolloClientUrl,
-//     }),
-//   });
-// });
