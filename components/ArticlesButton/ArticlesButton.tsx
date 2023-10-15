94e2bdc382c5561320ec9568d7e23deff7d802f8
Serhii Stanislav <serhiistanislav@gmail.com>
2023-10-15 19:16:12 +0300

    EDIT | bg

--- components/ArticlesButton/ArticlesButton.tsx
+++ components/ArticlesButton/ArticlesButton.tsx
@@ -41,6 +41,8 @@ const ArticlesButton = () => {
                 </svg>
               </div>
             </Link>
+
+            <span className={s.buttonBg} />
           </nav>
         </FixedContainer>
       )}
