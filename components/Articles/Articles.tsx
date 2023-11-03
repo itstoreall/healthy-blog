6dce589796c18fe2b3148541e3c6a03b3c57d5db
Serhii Stanislav <serhiistanislav@gmail.com>
2023-11-03 19:39:23 +0200

    EDIT | sort by date

--- components/Articles/Articles.tsx
+++ components/Articles/Articles.tsx
@@ -17,11 +17,11 @@ const Articles = ({ articles }: { articles: IArticle[] }) => {
   const { theme } = useTheme();
   const currentTheme = getCurrentTheme(theme);
 
-  const initialState = { label: "", order: "", articles };
-
   useEffect(() => {
-    console.log(0);
-    !arts && setArts(initialState);
+    const firstDate: Date = new Date(articles[0].timestamp);
+    const lastDate: Date = new Date(articles[articles.length - 1].timestamp);
+    const order = firstDate > lastDate ? "down" : "up";
+    !arts && setArts({ label: "", order, articles });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [articles]);
 
